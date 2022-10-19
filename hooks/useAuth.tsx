3c6d2609fbfaf76b1a1743785157abaaad12e84b
unknown <ulicmgh@gmail.com>
2022-10-19 10:12:52 -0300

    login firebase

---
+++ hooks/useAuth.tsx
@@ -0,0 +1,77 @@
+import { useRouter } from "next/router";
+import { createContext, useContext, useEffect, useMemo, useState } from "react";
+
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+  User,
+} from "firebase/auth";
+import { auth } from "../firebase";
+
+interface IAuth {
+  user: User | null;
+  signUp: (email: string, password: string) => Promise<void>;
+  signIn: (email: string, password: string) => Promise<void>;
+  logout: () => Promise<void>;
+  error: string | null;
+  loading: boolean;
+}
+
+interface AuthProviderProps {
+  children: React.ReactNode;
+}
+
+const AuthContext = createContext<IAuth>({
+  user: null,
+  signUp: async () => {},
+  signIn: async () => {},
+  logout: async () => {},
+  error: null,
+  loading: false,
+});
+
+const AuthProvider = ({ children }: AuthProviderProps) => {
+  const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState<User | null>(null);
+  const router = useRouter();
+
+  const signUp = async (email: string, password: string) => {
+    setLoading(true);
+    await createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredentials) => {
+        setUser(userCredentials.user);
+        router.push("/");
+        setLoading(false);
+      })
+      .catch((error) => alert(error.message))
+      .finally(() => setLoading(false));
+  };
+
+  const signIn = async (email: string, password: string) => {
+    setLoading(true);
+    await signInWithEmailAndPassword(auth, email, password)
+      .then((userCredentials) => {
+        setUser(userCredentials.user);
+        router.push("/");
+        setLoading(false);
+      })
+      .catch((error) => alert(error.message))
+      .finally(() => setLoading(false));
+  };
+
+  const logout = async () => {
+    setLoading(true);
+    signOut(auth)
+      .then(() => {
+        setUser(null);
+      })
+      .catch((error) => alert(error.message))
+      .finally(() => setLoading(false));
+  };
+
+  return <AuthContext.Provider>{children}</AuthContext.Provider>;
+};
+
+export default AuthProvider;
---
+++ pages/login.tsx
@@ -0,0 +1,101 @@
+import { signInAnonymously } from "firebase/auth";
+import Head from "next/head";
+import Image from "next/image";
+import React, { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+interface Inputs {
+  email: string;
+  password: string;
+}
+
+const login = () => {
+  const [login, setLogin] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = async ({ email, password }) => {
+    if (login) {
+      // await signIn(email, password);
+    } else {
+      // await signUp(email, password);
+    }
+  };
+
+  return (
+    <div className="relative flex h-screen w-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent">
+      <Head>
+        <title>Videogames Remaster</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Image
+        src="https://staticg.sportskeeda.com/editor/2022/01/f6fc4-16428755317343-1920.jpg"
+        layout="fill"
+        className="-z-10 !hidden opacity-60 sm:!inline"
+        objectFit="cover"
+      />
+
+      {/* Imagen logo de la pagina */}
+
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14 "
+      >
+        <h1 className="text-4xl text-blue-700 font-semibold ">Sign In</h1>
+
+        <div className="space-y-4 ">
+          <label className="inline-block w-full">
+            <input
+              type="email"
+              placeholder="Email"
+              className="input"
+              {...register("email", { required: true })}
+            />
+            {errors.email && (
+              <p className="p-1 text-[13px]  text-red-500">
+                Please enter a valid Email.
+              </p>
+            )}
+          </label>
+          <label className="inline-block w-full">
+            <input
+              type="password"
+              placeholder="Password"
+              className="input"
+              {...register("password", { required: true })}
+            />
+            {errors.email && (
+              <p className="p-1 text-[13px]  text-red-500">
+                Please enter a valid Password.
+              </p>
+            )}
+          </label>
+        </div>
+
+        <button
+          type="submit"
+          className="w-full rounded bg-[#0970e5] py-3 font-semibold"
+          onClick={() => setLogin(true)}
+        >
+          Sign In
+        </button>
+
+        <div>
+          New User?{"  "}
+          <button
+            className="text-blue-700 hover:underline"
+            onClick={() => setLogin(false)}
+          >
+            Sign Up Now
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default login;
